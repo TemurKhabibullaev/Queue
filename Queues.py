fc37242fca88d30bf45de37f1308a37b3afacdb7
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-03-04 14:10:32 -0500

    Learning

--- Queues.py
+++ Queues.py
@@ -1,5 +1,9 @@
-from LinkedList import SinglyLinkedList
-instance = SinglyLinkedList()
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 
 class Queues:
@@ -8,8 +12,27 @@ class Queues:
         self.last = None
 
     def enqueue(self, data):
-        instance.add_end(data)
+        new_node = Node(data)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
 
     def dequeue(self):
-        instance.del_head()
+        ret = self.head.data
+        self.head = self.head.next
+        return ret
+
+    def display(self):
+        elems = []
+        current = self.head
+        elems.append(current.data)
+        while current.next:
+            current = current.next
+            elems.append(current.data)
+        return elems
+
 
